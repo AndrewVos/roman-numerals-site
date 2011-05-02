c0d6bf58baae9e3f1b274d2e2cdff7a1ad968b42
Andrew Vos <andrew.vos@gmail.com>
2011-05-02 17:15:51 +0100

    added a 'post to twitter' button

--- test/functional/home_controller_test.rb
+++ test/functional/home_controller_test.rb
@@ -15,4 +15,14 @@ class HomeControllerTest < ActionController::TestCase
     get :index, {'value' => 'XXIII'}
     assert_equal 23, assigns(:converted)
   end
+
+  test "should not show a 'post on twitter' link if nothing has been converted" do
+    get :index, {'value' => '23'}
+    assert_select ['a', false], :text => 'Share your results on Twitter!'
+  end
+
+  test "should show a 'post on twitter' link" do
+    get :index, {'value' => '23'}
+    assert_select 'a', :text => 'Share your results on Twitter!'
+  end
 end
