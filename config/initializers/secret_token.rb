3b72342bde5223c1506a8b8344ef336ca9777a73
Andrew Vos <andrew.vos@gmail.com>
2011-05-02 13:36:39 +0100

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+RomanNumerals::Application.config.secret_token = '59e70f9e3e1a9d1608e065f8a15ce3577d0796e315d39f1f66c6fd021a13c9943abae2138baedd0be8e10e477eda6dbdb7e5f532110a57c36d3ceb26048c907a'
